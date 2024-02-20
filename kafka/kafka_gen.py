31cb0b3a2b5d8b91b824fb253a844e9d0a070f58
Matthew Tan <mtwtan@gmail.com>
2024-02-20 08:11:54 -0500

    Update kafka_gen.py

--- kafka/kafka_gen.py
+++ kafka/kafka_gen.py
@@ -35,15 +35,29 @@ def json_serial(obj):
     raise TypeError ("Type %s not serializable" % type(obj))
 
 def genFakeData():
-    event_time = datetime.now().isoformat()
+
+    coffee = ('pour','latte','mocha')
+
+    coffee_prod_price_dict = {
+        'pour':2.50,
+        'latte': 4.50,
+        'mocha': 5.00,
+    }
+    
+    myuuid = uuid.uuid4()
+    event_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     name = fake.name()
     address = fake.address()
     tel = fake.phone_number()
-    ssn = fake.ssn()
     aba = fake.aba()
     bankaccount = fake.bban()
     ccdetail = fake.credit_card_full()
     
+    product = fake.random_element(elements=coffee)
+    pricedollar = coffee_prod_price_dict[product]
+    number = fake.random_int(min=1, max=10)
+    total_amount = pricedollar * number
+        
     day_random_diff = randint(0,5)
     current_dateTime = datetime.now()
     dateTime_delta = timedelta(days = day_random_diff)
@@ -53,78 +67,60 @@ def genFakeData():
     year = dateTime.year
 
     d = {
+      "uuid": str(myuuid),
       "event_time": event_time,
       "name": name,
       "address": address,
       "tel": tel,
-      "ssn": ssn,
       "aba": aba,
       "bankaccount": bankaccount,
       "creditcardinfo": ccdetail,
-      "datereceived": current_dateTime,
+      "datereceived": dateTime.strftime("%Y-%m-%d %H:%M:%S"),
       "r_year": year,
       "r_month": month,
-      "r_day": day_of_month
+      "r_day": day_of_month,
+      "product": product,
+      "number": number,
+      "total_amount": total_amount
     }
 
     return d
 
-#print(genFakeData())
+def main():
+    numloop = randint(50,100)
+    
+    tp = MSKTokenProvider()
+    
+    print("Bootstrap server for Kafka Producer: ", BOOTSTRAP_SERVERS)
+    producer = KafkaProducer(
+        bootstrap_servers=BOOTSTRAP_SERVERS,
+        security_protocol='SASL_SSL',
+        sasl_mechanism='OAUTHBEARER',
+        sasl_oauth_token_provider=tp,
+        client_id=socket.gethostname(),
+    )
+    
+    topic = "test-topic"
+    counter = 0
+    
+    for i in range(numloop):
+        try:
+            key = str(counter).encode()
+            each_msg = json.dumps(genFakeData(), default=json_serial).encode('utf-8')
+    
+            producer.send(topic,key=key,value=each_msg)
+            producer.flush()
+            print("Produced: ",key, "\n",each_msg)
+        except Exception:
+            print("Failed to send: ", key, "\n", each_msg, "\n")
+    
+        counter+=1
 
-#print(randint(10,100))
+    time.sleep(5)
+    producer.close()
 
-numloop = randint(50,100)
+### Start of code
 
-#msg = []
-
-#count = 0
-#for i in range(numloop):
-
-#  each_msg = genFakeData()
-#  msg.append(each_msg)
-#  key = str(count).encode()
-#  print(key)
-#  count+=1
-
-#exit()
-
-
-tp = MSKTokenProvider()
-
-print("Bootstrap server for Kafka Producer: ", BOOTSTRAP_SERVERS)
-producer = KafkaProducer(
-    bootstrap_servers=BOOTSTRAP_SERVERS,
-    security_protocol='SASL_SSL',
-    sasl_mechanism='OAUTHBEARER',
-    sasl_oauth_token_provider=tp,
-    client_id=socket.gethostname(),
-)
-
-topic = "test-topic"
-counter = 0
-
-#while True:
-#    try:
-#        inp=input(">")
-#        producer.send(topic, inp.encode())
-#        producer.flush()
-#        print("Produced!")
-#    except Exception:
-#        print("Failed to send message:", e)
-
-for i in range(numloop):
-    try:
-        key = str(counter).encode()
-        each_msg = json.dumps(genFakeData(), default=json_serial).encode('utf-8')
- 
- #data = json.dumps(chunkd, default=str).encode('utf-8')
-
-        producer.send(topic,key=key,value=each_msg)
-        producer.flush()
-        print("Produced: ",key, "\n",each_msg)
-    except Exception:
-        print("Failed to send: ", key, "\n", each_msg, "\n")
-
-    counter+=1
-
-producer.close()
+if __name__ == "__main__":
+    while 1 == 1:
+        main()
